f2e30a4a26bfdc3c9572d53a0df127d311e2c64d
David Njuguna <dnjuguna@gmail.com>
2014-09-03 21:32:06 +0300

    refactoring

--- context.js
+++ context.js
@@ -5,7 +5,11 @@ module.exports = function(){
 	var self = {};
 
 	return {
-		init: function(settings){
+		init: function(){
+			self.store = {};
+			return this;
+		},
+		destroy: function(){
 			self.store = {};
 			return this;
 		},
@@ -14,37 +18,21 @@ module.exports = function(){
 			return this;
 		},
 		get : function(name) {
-			return self.store[name] ? self.store[name] : null;
+			return self.store.hasOwnProperty(name) ? self.store[name] : null;
 		},
-		statusCode: function(){
-			var statusCode =  arguments[0];
-			if(statusCode)
-				self.statusCode = statusCode;
-			return self.statusCode;
-		},
-		header: function(){
-			var key = arguments[0].toLowerCase();
-			var value = arguments[1];
-			if(value)
-				self.headers[key] = value;
-			else
-				return self.headers[key];
-			return this;
-		},
-		headers: function(){
-			return self.headers;
-		},
-		publish: function(error, data){
-			if(error)
-				data.data = error;
-			this.get("gereji-publisher").push(data.app, data.handler, data.data);
+		publish: function(){
 			this.set("queue", (this.get("queue") - 1));
-            if(this.get("route").sync)
-                if(this.get("queue") > 0)
-                    return this;
-            var publisher = this.get("publisher");
-            publisher.write(this);
-			return this;
+			if(this.get("queue") > 0)
+				return this;
+			this.get("publisher").write(this);
+		},
+		redirect: function(url){
+			var publisher = this.get("publisher");
+            publisher.header("Location", url);
+            publisher.statusCode(302);
+            publisher.writeHead(this);
+            this.get("response").end();
+			this.destroy();
 		},
 		log: function(severity, message){
 			var logfile = self.store.settings.path + self.store.settings.debug.file;
@@ -53,11 +41,10 @@ module.exports = function(){
 			if(severity > self.store.settings.debug.level)
 				return;
 			var line = (new Date()) + ' (' + severity + ') : [' + uri + '] ' + ' {' + method + '} ' + (JSON.stringify(message)) + '\n';
-			fs.appendFile(logfile, line, function(){});
+			fs.appendFile(logfile, line, function(){
+				console.log(line);
+			});
 			return this;
-		},
-		require: function(){
-			return require(self.store["settings"].path + arguments[0]);
 		}
 	}
 };
